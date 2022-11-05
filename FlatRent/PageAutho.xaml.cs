94ef7f6dfeabea60dab2aa4baec1c8f852684150
Николаев Клементий <klim_nikolaev03@mail.ru>
2022-11-05 22:49:12 +0300

    Добавьте файлы проекта.

---
+++ FlatRent/PageAutho.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FlatRent
+{
+    /// <summary>
+    /// Логика взаимодействия для PageAutho.xaml
+    /// </summary>
+    public partial class PageAutho : Page
+    {
+        public PageAutho()
+        {
+            InitializeComponent();
+        }
+
+        private void arendator_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void hozain_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
